248cead77a6e148dbe3fab51492776841ad6f64d
plima94 <pauloalima1994@gmail.com>
2022-12-20 11:58:07 +0000

    queue commit

---
+++ queue_ll.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def is_empty(self):
+        return not self.items
+        # return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.popleft()
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[0]
+
+    def __str__(self):
+        return str(self.items)
